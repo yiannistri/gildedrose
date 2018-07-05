75efd582e187314dcdef2a48e916af859355de84
Ivan Moore <ivan@teamoptimization.com>
2018-07-05 21:15:19 +0100

    AgedBrie

--- src/main/java/com/gildedrose/AgedBrie.java
+++ src/main/java/com/gildedrose/AgedBrie.java
@@ -1,7 +1,32 @@
 package com.gildedrose;
 
-public class AgedBrie extends EverythingGildedRoseItem {
+public class AgedBrie implements GildedRoseItem {
+    public final Item item;
+
     public AgedBrie(Item item) {
-        super(item);
+        this.item = item;
+    }
+
+    @Override
+    public void updateQuality() {
+        ageWell(item);
+
+        item.sellIn = item.sellIn - 1;
+
+        if (hasExpired(item)) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private boolean hasExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void ageWell(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
     }
 }
