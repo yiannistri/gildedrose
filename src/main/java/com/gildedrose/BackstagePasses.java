fa80a8ed8b9bc5882310f5fa8891d4bce36d249e
Ivan Moore <ivan@teamoptimization.com>
2018-07-05 21:16:44 +0100

    BackstagePasses

--- src/main/java/com/gildedrose/BackstagePasses.java
+++ src/main/java/com/gildedrose/BackstagePasses.java
@@ -1,7 +1,42 @@
 package com.gildedrose;
 
-public class BackstagePasses extends EverythingGildedRoseItem {
+public class BackstagePasses implements GildedRoseItem {
+    public final Item item;
+
     public BackstagePasses(Item item) {
-        super(item);
+        this.item = item;
+    }
+
+    @Override
+    public void updateQuality() {
+        ageWell(item);
+
+        item.sellIn = item.sellIn - 1;
+
+        if (hasExpired(item)) {
+            item.quality = 0;
+        }
+    }
+
+    private boolean hasExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void ageWell(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
     }
 }
