2430d2d3794d76b130edb555ef933a928b051037
Ivan Moore <ivan@teamoptimization.com>
2018-07-05 21:18:18 +0100

    OtherItem

--- src/main/java/com/gildedrose/OtherItem.java
+++ src/main/java/com/gildedrose/OtherItem.java
@@ -1,7 +1,30 @@
 package com.gildedrose;
 
-public class OtherItem extends EverythingGildedRoseItem {
+public class OtherItem implements GildedRoseItem {
+    public final Item item;
+
     public OtherItem(Item item) {
-        super(item);
+        this.item = item;
+    }
+
+    @Override
+    public void updateQuality() {
+        ageBadly(item);
+
+        item.sellIn = item.sellIn - 1;
+
+        if (hasExpired(item)) {
+            ageBadly(item);
+        }
+    }
+
+    private boolean hasExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void ageBadly(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
     }
 }
