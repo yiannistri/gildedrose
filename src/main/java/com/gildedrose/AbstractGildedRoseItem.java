e81e6e266304dda8c882656cd851ad9627768c58
Ivan Moore <ivan@teamoptimization.com>
2018-07-26 23:28:41 +0100

    message

--- src/main/java/com/gildedrose/AbstractGildedRoseItem.java
+++ src/main/java/com/gildedrose/AbstractGildedRoseItem.java
@@ -1,38 +1,37 @@
 package com.gildedrose;
 
 public abstract class AbstractGildedRoseItem implements GildedRoseItem {
-    final Item item;
+    private final Item item;
 
     AbstractGildedRoseItem(Item item) {
         this.item = item;
     }
 
-    public void setNoQuality() {
+    void setNoQuality() {
         item.quality = 0;
     }
 
-    public void decrementSellIn() {
+    void decrementSellIn() {
         item.sellIn = item.sellIn - 1;
     }
 
-    public boolean hasExpired() {
+    boolean hasExpired() {
         return expiresBy(0);
     }
 
-    public boolean expiresBy(int expiryTime) {
+    boolean expiresBy(int expiryTime) {
         return item.sellIn < expiryTime;
     }
 
-    public void decrementQuality() {
+    void decrementQuality() {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
     }
 
-    public void incrementQuality() {
+    void incrementQuality() {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
     }
-
 }
