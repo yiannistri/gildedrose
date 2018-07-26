656abca24103064045980e974b635ae5f0d06779
Ivan Moore <ivan@teamoptimization.com>
2018-07-26 23:10:45 +0100

    message

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,64 +9,69 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (isAgedBrie(item) || isBackstagePasses(item)) {
-                incrementQuality(item);
-                if (isBackstagePasses(item)) {
-                    if (item.sellIn < 11) {
-                        incrementQuality(item);
-                    }
+            updateQuality(new GildedRoseItem(item));
+        }
+    }
 
-                    if (item.sellIn < 6) {
-                        incrementQuality(item);
-                    }
-                }
-            } else {
-                if (!isSulfuras(item)) {
-                    decrementQuality(item);
-                }
-            }
-
-            if (!isSulfuras(item)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (isAgedBrie(item)) {
+    public void updateQuality(GildedRoseItem gildedRoseItem) {
+        Item item = gildedRoseItem.item;
+        if (isAgedBrie(item) || isBackstagePasses(item)) {
+            incrementQuality(item);
+            if (isBackstagePasses(item)) {
+                if (item.sellIn < 11) {
                     incrementQuality(item);
+                }
+
+                if (item.sellIn < 6) {
+                    incrementQuality(item);
+                }
+            }
+        } else {
+            if (!isSulfuras(item)) {
+                decrementQuality(item);
+            }
+        }
+
+        if (!isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (isAgedBrie(item)) {
+                incrementQuality(item);
+            } else {
+                if (isBackstagePasses(item)) {
+                    item.quality = 0;
                 } else {
-                    if (isBackstagePasses(item)) {
-                        item.quality = 0;
-                    } else {
-                        if (!isSulfuras(item)) {
-                            decrementQuality(item);
-                        }
+                    if (!isSulfuras(item)) {
+                        decrementQuality(item);
                     }
                 }
             }
         }
     }
 
-    public void decrementQuality(Item item) {
+    public static void decrementQuality(Item item) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
     }
 
-    public void incrementQuality(Item item) {
+    public static void incrementQuality(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
     }
 
-    public boolean isSulfuras(Item item) {
+    public static boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
-    public boolean isBackstagePasses(Item item) {
+    public static boolean isBackstagePasses(Item item) {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
-    public boolean isAgedBrie(Item item) {
+    public static boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
     }
 }
---
+++ src/main/java/com/gildedrose/GildedRoseItem.java
@@ -0,0 +1,9 @@
+package com.gildedrose;
+
+public class GildedRoseItem {
+    final Item item;
+
+    public GildedRoseItem(Item item) {
+        this.item = item;
+    }
+}
